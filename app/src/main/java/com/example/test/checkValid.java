2c8cb4d76a1615068cef9bb0b51a5e34b6a58080
CalciumPotato <behjunling@hotmail.com>
2023-08-07 01:54:56 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/test/checkValid.java
@@ -0,0 +1,30 @@
+package com.example.test;
+
+import android.widget.TextView;
+
+public class checkValid {
+
+    public static boolean checkNumber(String num)
+    {
+        if (num.isEmpty() || Float.parseFloat(num) <= 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkPercentage(String pcnt)
+    {
+        if (pcnt.isEmpty() || Float.parseFloat(pcnt) <= 0 || Float.parseFloat(pcnt) > 100)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void outputNumberInvalidity(TextView tv, String str)
+    {
+        tv.setText("Please enter a valid " + str + ".");
+    }
+
+}
